88eae85ca65ad7a5472d13c4ce9aec8b60ed8d5f
Karl Yerkes <karl.yerkes@gmail.com>
2014-09-10 16:09:59 -0700

    attempt at serving files except if requests begin with "/wiki"

--- server.js
+++ server.js
@@ -1,13 +1,36 @@
+// wiki server
+//
 
 var connect = require('connect'),
+    fs = require('fs'),
     sharejs = require('share').server;
 
-// XXX do we have to serve more than one page?
-// i think one page (index.html) might do it, but i'm note sure.
+// XXX maybe, eventually we want to make this live with a file-watcher
 //
-var server = connect(connect.logger(), connect.static(__dirname + '/public'));
+// server a single page
+//
+var indexContent = fs.readFileSync('public/index.html');
+var server = connect(connect.logger(), function(request, response){
+  var fun = connect.static(__dirname + '/public');
+  // XXX - look at the request.
+  //
+  // if it starts 'wiki' then serve the index content. otherwise, serve files
+  // normally.
+  //
+  //console.log(request);
+
+  if (request.url.indexOf("/wiki") == -1) {
+    console.log("got here");
+    fun(request, response);
+  }
+  else {
+    response.writeHead(200, {'Content-Type': 'text/plain'});
+    response.end(indexContent);
+  }
+});
 
 // XXX enable persistance
+//
 // we should save the contents of the ShareJS "context" at the url path. we
 // could use a nosql database with the url path as the key and the ShareJS
 // buffer contents as the value.
@@ -20,6 +43,8 @@ var options = {db: {type: 'none'}};
 //
 sharejs.attach(server, options);
 
+// XXX - eventually share on port 80
+//
 server.listen(8000, function(){
     console.log('Server running at http://127.0.0.1:8000/');
 });
