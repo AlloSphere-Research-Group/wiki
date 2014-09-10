d3169fbef39cc33c5623350a7bfa02f4235fc266
Karl Yerkes <karl.yerkes@gmail.com>
2014-09-10 14:09:40 -0700

    added some comments

---
+++ README.md
@@ -0,0 +1,23 @@
+# Wiki
+
+This project will provide a real-time collaborative wiki based on [Markdown][],
+and [Operational Transformation][] like the [ShareJS wiki example][].
+
+## Installation and Deployment
+
+    ./install_dependencies.sh
+    node server.js
+
+## Links and References
+
+[ShareJS wiki example][]
+[Markdown][]
+[Operational Transformation][]
+[ShareJS][]
+[Showdown][]
+
+[ShareJS wiki example]: http://sharejs.org/wiki/Main
+[Markdown]: http://daringfireball.net/projects/markdown/syntax
+[Operational Transformation]: http://en.wikipedia.org/wiki/Operational_transformation
+[ShareJS]: http://sharejs.org
+[Showdown]: https://github.com/coreyti/showdown
--- server.js
+++ server.js
@@ -1,13 +1,23 @@
+
 var connect = require('connect'),
     sharejs = require('share').server;
 
-var server = connect(
-      connect.logger(),
-      connect.static(__dirname + '/public')
-    );
-var options = {db: {type: 'none'}}; // See docs for options. {type: 'redis'} to enable persistance.
+// XXX do we have to serve more than one page?
+// i think one page (index.html) might do it, but i'm note sure.
+//
+var server = connect(connect.logger(), connect.static(__dirname + '/public'));
+
+// XXX enable persistance
+// we should save the contents of the ShareJS "context" at the url path. we
+// could use a nosql database with the url path as the key and the ShareJS
+// buffer contents as the value.
+//
+// See docs for options. {type: 'redis'} to enable persistance.
+//
+var options = {db: {type: 'none'}};
 
 // Attach the sharejs REST and Socket.io interfaces to the server
+//
 sharejs.attach(server, options);
 
 server.listen(8000, function(){
