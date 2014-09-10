2f059f49115f1b25a0d9b74f03f3fdffc31b925a
Karl Yerkes <karl.yerkes@gmail.com>
2014-09-10 13:34:48 -0700

    initial attempt

---
+++ install_dependencies.sh
@@ -0,0 +1,16 @@
+#!/bin/sh
+
+if [ `which apt-get 2>/dev/null` ]; then
+  echo "TBD - add add support for Ubuntu/Debian Linux"
+elif [ `which pacman 2>/dev/null` ]; then
+  echo "TBD - add support for Arch Linux"
+elif [ `which brew 2>/dev/null` ]; then
+  npm install share@"<0.7"
+  npm install connect@"<2.0.0"
+  brew install redis
+  npm install -g redis
+elif [ `which port 2>/dev/null` ]; then
+  echo "TBD - add support for Macports"
+else
+  echo "ERROR - your system is unknown to us"
+fi
