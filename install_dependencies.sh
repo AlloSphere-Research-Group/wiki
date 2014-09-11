670f3e7b99f247508246673dc268e108cb1a21ae
hannahwolfe <hanawolfe@gmail.com>
2014-09-11 08:48:57 -0700

    "dependency stuff for ubuntu, must manually install redis first, also need to sudo ./install_dependencies.sh

--- install_dependencies.sh
+++ install_dependencies.sh
@@ -1,7 +1,11 @@
 #!/bin/sh
 
 if [ `which apt-get 2>/dev/null` ]; then
-  echo "TBD - add add support for Ubuntu/Debian Linux"
+  echo "install regis first, need to sudo"
+  npm install share@"<0.7"
+  npm install connect@"<2.0.0"
+  npm install -g redis
+
 elif [ `which pacman 2>/dev/null` ]; then
   echo "TBD - add support for Arch Linux"
 elif [ `which brew 2>/dev/null` ]; then
