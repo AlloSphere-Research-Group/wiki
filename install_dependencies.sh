b8a94eb3f1471a41bac063e22b9ab2d2364ff4d5
Karl Yerkes <karl.yerkes@gmail.com>
2014-09-12 14:23:01 -0700

    modifications to install script

--- install_dependencies.sh
+++ install_dependencies.sh
@@ -2,16 +2,20 @@
 
 if [ `which apt-get 2>/dev/null` ]; then
   echo "install regis first, need to sudo"
-  npm install share@"<0.7"
-  npm install connect@"<2.0.0"
-  npm install -g redis
-
+  sudo npm install share@"<0.7"
+  sudo npm install connect@"<2.0.0"
+  sudo npm install -g redis
 elif [ `which pacman 2>/dev/null` ]; then
   echo "TBD - add support for Arch Linux"
 elif [ `which brew 2>/dev/null` ]; then
+  if [ "$(whoami)" == "root" ]; then
+    echo "Sorry, you are root. Don't ever 'sudo brew ...'"
+    exit 1
+  fi
+  brew install node
+  brew install redis
   npm install share@"<0.7"
   npm install connect@"<2.0.0"
-  brew install redis
   npm install -g redis
 elif [ `which port 2>/dev/null` ]; then
   echo "TBD - add support for Macports"
